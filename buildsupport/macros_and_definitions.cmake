6b77d0de4076f2bca366146f8965e624fffbf4b9
Gabriel Ferreira <gabrielcarvfer@gmail.com>
2017-10-30 01:26:33 -0200

    Update macros_and_definitions.cmake

--- buildsupport/macros_and_definitions.cmake
+++ buildsupport/macros_and_definitions.cmake
@@ -1,3 +1,15 @@
+if (WIN32)
+    #If using MSYS2
+    set(MSYS2_PATH "E:\\msys64\\mingw64")
+    set(GTK2_GDKCONFIG_INCLUDE_DIR "${MSYS2_PATH}\\include\\gtk-2.0")
+    set(GTK2_GLIBCONFIG_INCLUDE_DIR "${MSYS2_PATH}\\include\\gtk-2.0")
+    set(QT_QMAKE_EXECUTABLE "${MSYS2_PATH}\\bin\\qmake.exe")
+    set(QT_RCC_EXECUTABLE "${MSYS2_PATH}\\bin\\rcc.exe")
+    set(QT_UIC_EXECUTABLE "${MSYS2_PATH}\\bin\\uic.exe")
+    set(QT_MOC_EXECUTABLE "${MSYS2_PATH}\\bin\\moc.exe")
+    set(QT_MKSPECS_DIR    "${MSYS2_PATH}\\share\\qt4\\mkspecs")
+endif()
+
 #Fixed definitions
 unset(CMAKE_LINK_LIBRARY_SUFFIX)
 
@@ -12,11 +24,12 @@ add_definitions(-DNS_TEST_SOURCEDIR="${CMAKE_OUTPUT_DIRECTORY}/test")
 #process all options passed in main cmakeLists
 macro(process_options)
     #Copy all header files to outputfolder/include/
-    FILE(GLOB_RECURSE include_files ${PROJECT_SOURCE_DIR}/*.h) #just copying every single header into ns3 include folder
+    #just copying every single header in src to include folder
+        #contribution headers are copied somewhere else
+    FILE(GLOB_RECURSE include_files ${PROJECT_SOURCE_DIR}/src/*.h)
     file(COPY ${include_files} DESTINATION ${CMAKE_HEADER_OUTPUT_DIRECTORY})
 
     #Set common include folder
-    include_directories( ${CMAKE_OUTPUT_DIRECTORY})
     include_directories(${CMAKE_OUTPUT_DIRECTORY})
 
     #Set C++ standard
@@ -43,7 +56,7 @@ macro(process_options)
         else()
             link_directories(${GTK2_LIBRARY_DIRS})
             include_directories( ${GTK2_INCLUDE_DIRS})
-            add_definitions(${GTK2_DEFINITIONS})
+            add_definitions(${GTK2_CFLAGS_OTHER})
         endif()
     endif()
 
@@ -84,7 +97,7 @@ macro(process_options)
     if(${NS3_MPI})
         find_package(MPI)
         if(NOT ${MPI_FOUND})
-            message(FATAL_ERROR "MPI not found")
+            #message(FATAL_ERROR "MPI not found")
         else()
             include_directories( ${MPI_INCLUDE_PATH})
             add_definitions(${MPI_COMPILE_FLAGS} ${MPI_LINK_FLAGS})
@@ -100,6 +113,17 @@ macro(process_options)
             link_directories(${GSL_LIBRARY})
         endif()
     endif()
+
+    #if(${NS3_GNUPLOT})
+    #    find_package(Gnuplot-ios)
+    #    if(NOT ${GNUPLOT_FOUND})
+    #        message(FATAL_ERROR GNUPLOT not found)
+    #    else()
+    #        include_directories(${GNUPLOT_INCLUDE_DIRS})
+    #        link_directories(${GNUPLOT_LIBRARY})
+    #    endif()
+    #endif()
+
 
     #process debug switch
     if(${NS3_DEBUG})
@@ -159,9 +183,12 @@ macro(process_options)
         list(APPEND ns3-libs ${lib${libname}})
     endforeach()
 
+    #Dump definitions for later use
+    get_directory_property( ADDED_DEFINITIONS COMPILE_DEFINITIONS )
+    file(WRITE ${CMAKE_HEADER_OUTPUT_DIRECTORY}/ns3-definitions "${ADDED_DEFINITIONS}")
 endmacro()
 #----------------------------------------------
-macro (write_module_header name header_files)
+macro (write_module_header name header_files contrib_name)
     string(TOUPPER ${name} uppercase_name)
     string(REPLACE "-" "_" final_name ${uppercase_name} )
     #Common module_header
@@ -176,9 +203,20 @@ macro (write_module_header name header_files)
     list(APPEND contents "
     // Module headers: ")
 
+    #check if contrib
+    string(LENGTH "${contrib_name}" CONTRIB_LEN)
+
     #Write each header listed to the contents variable
     foreach(header ${header_files})
+        #Get file name
         get_filename_component(head ${header} NAME)
+
+        #append contrib path if contrib
+        if(${CONTRIB_LEN} GREATER 0)
+            set(head "${contrib_name}/${head}")
+        endif()
+
+        #append path of header to contents of the module include
         list(APPEND contents
                 "
     #include \"${head}\"")
@@ -191,28 +229,35 @@ macro (write_module_header name header_files)
 endmacro()
 
 
-macro (build_lib name source_files header_files libraries_to_link test_sources)
+macro (build_lib libname source_files header_files libraries_to_link test_sources)
     #Create shared library with sources and headers
-    add_library(${lib${name}} SHARED "${source_files}" "${header_files}")
+    add_library(${lib${libname}} SHARED "${source_files}" "${header_files}")
 
     #Link the shared library with the libraries passed
-    target_link_libraries(${lib${name}} ${libraries_to_link})
+    target_link_libraries(${lib${libname}} ${libraries_to_link})
 
     #Write a module header that includes all headers from that module
-    write_module_header("${name}" "${header_files}")
+    write_module_header("${libname}" "${header_files}" "")
 
     #Build tests if requested
     if(${NS3_TESTS})
         list(LENGTH test_sources test_source_len)
         if (${test_source_len} GREATER 0)
-            #Create name of output library test of module
-            list(APPEND test${name} ns${NS3_VER}-${name}-test-${build_type})
+            #Create libname of output library test of module
+            list(APPEND test${libname} ns${NS3_VER}-${libname}-test-${build_type})
 
             #Create shared library containing tests of the module
-            add_library(${test${name}} SHARED "${test_sources}")
+            add_library(${test${libname}} SHARED "${test_sources}")
 
             #Link test library to the module library
-            target_link_libraries(${test${name}} ${lib${name}})
+            target_link_libraries(${test${libname}} ${lib${libname}})
+        endif()
+    endif()
+
+    #Build lib examples if requested
+    if(${NS3_EXAMPLES})
+        if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/examples)
+            add_subdirectory(examples)
         endif()
     endif()
 
@@ -221,12 +266,12 @@ macro (build_lib name source_files header_files libraries_to_link test_sources)
         set(arch gcc_ILP32)
         add_custom_command(
                 OUTPUT
-                ${PROJECT_SOURCE_DIR}/src/${name}/bindings/modulegen_${arch}.py
+                ${PROJECT_SOURCE_DIR}/src/${libname}/bindings/modulegen_${arch}.py
                 COMMAND
                 ${PROJECT_SOURCE_DIR}/bindings/python/ns3modulegen-modular.py
-                ${PROJECT_SOURCE_DIR}/src/${name}/bindings/
-                ${PROJECT_SOURCE_DIR}/src/${name}/
-                ${CMAKE_HEADER_OUTPUT_DIRECTORY}/${name}-module.h
+                ${PROJECT_SOURCE_DIR}/src/${libname}/bindings/
+                ${PROJECT_SOURCE_DIR}/src/${libname}/
+                ${CMAKE_HEADER_OUTPUT_DIRECTORY}/${libname}-module.h
                 modulegen_${arch}.py
                 ${CMAKE_CXX_FLAGS}
                 )
@@ -242,7 +287,20 @@ macro (build_example name source_files header_files libraries_to_link)
 
     set_target_properties( ${name}
             PROPERTIES
-            RUNTIME_OUTPUT_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/examples
+            RUNTIME_OUTPUT_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/examples/${examplename}
+            )
+endmacro()
+
+macro (build_lib_example name source_files header_files libraries_to_link)
+    #Create shared library with sources and headers
+    add_executable(${name} "${source_files}" "${header_files}")
+
+    #Link the shared library with the libraries passed
+    target_link_libraries(${name} ${libraries_to_link})
+
+    set_target_properties( ${name}
+            PROPERTIES
+            RUNTIME_OUTPUT_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/examples/${libname}
             )
 endmacro()
 
